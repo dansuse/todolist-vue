fbfc9703f6ae056886d4e818ab29a7e146e6379f
Daniel Sugiarto Setyabudi <danielsugiarto89@gmail.com>
2020-05-17 16:33:29 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  },
+  data: function () {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>Completed Tasks: {{ todos.filter(todo => {return todo.done === true}).length }}</p>
+        <p>Pending Tasks: {{ todos.filter(todo => {return todo.done === false}).length }}</p>
+        <Todo v-on:delete-todo='deleteTodo' v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index">
+        </Todo>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+import Todo from './Todo'
+
+export default{
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
