5f25467e47220518249f31a01cf6f0405b8f0508
Daniel Sugiarto Setyabudi <danielsugiarto89@gmail.com>
2020-05-20 22:17:46 +0800

    Implement feature to change todo status from pending to complete.

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -37,7 +37,7 @@
         <div class='ui bottom attached green basic button' v-show="!isEditing && todo.done" disabled>
             Completed
         </div>
-        <div class='ui bottom attached red basic button' v-show='!isEditing && !todo.done'>
+        <div class='ui bottom attached red basic button' v-show='!isEditing && !todo.done' v-on:click="completeTodo(todo)">
             Pending
         </div>
     </div>
@@ -62,6 +62,9 @@ export default{
     },
     deleteTodo (todo) {
       this.$emit('delete-todo', todo)
+    },
+    completeTodo (todo) {
+      this.$emit('complete-todo', todo)
     }
   }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Completed Tasks: {{ todos.filter(todo => {return todo.done === true}).length }}</p>
         <p>Pending Tasks: {{ todos.filter(todo => {return todo.done === false}).length }}</p>
-        <Todo v-on:delete-todo='deleteTodo' v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index">
+        <Todo v-on:complete-todo='completeTodo' v-on:delete-todo='deleteTodo' v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index">
         </Todo>
     </div>
 </template>
@@ -20,6 +20,10 @@ export default{
     deleteTodo (todo) {
       const todoIndex = this.todos.indexOf(todo)
       this.todos.splice(todoIndex, 1)
+    },
+    completeTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = true
     }
   }
 }
